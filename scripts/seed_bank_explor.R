aad4d0dd6b89bf19978096bd153c3233fc46cd76
ShaunCoutts <shaun.coutts@gmail.com>
2016-05-27 12:26:14 +0100

    adds the seeb bank data, dispersal data and a very early R script

---
+++ scripts/seed_bank_explor.R
@@ -0,0 +1,48 @@
+#seed bank overlap
+sb <- read.csv('/home/shauncoutts/Dropbox/projects/seed_bank/SEEDBANK.CSV', header = T)
+ob_name <- load("/home/shauncoutts/Dropbox/projects/COMPADRE_spatial_replication/Data/Demography/Rlease_version/COMPADRE Jul 21 2014.RData")
+
+comp_spp <- unique(gsub('_', x = compadre$metadata$SpeciesAccepted, ' '))
+sb_spp <- unique(as.character(sb$Species))
+
+overlap <- comp_spp[comp_spp %in% sb_spp ] # 92 spp overlap
+
+#dispersal data
+disp <- read.csv('/home/shauncoutts/Dropbox/projects/seed_bank/DispersalDistanceData.csv', header = TRUE) 
+d_spp <- as.character(disp$Species)
+overlap_disp <- d_spp[d_spp %in%  overlap] #43 spp overlap
+
+
+#Black grass seed bank
+sb[grepl('Alopecurus myosuroides', sb$Species), ]
+#Information extracted from seed bank data base to look at black grass seed banks 
+
+#interpriting seed longevity codes
+# 1: seeds survive <1 year
+# 2: seeds surviv >1 and <5 years
+# 3: seeds survive >5 years
+# 4: not able to determine
+
+#interpriting methodology code
+# 1: seeds buried in garden plot, no additional disturbance
+# 2: seeds buried in garden plot, additional disturbance
+# 3: deliberate burial in field
+# 4: field sample taken and seeds extracted and tested for germination or viability
+# 5: soil sample taken from field, seeds germinated without extraction
+# 6: as 5 but seeds germinated in the field
+# 7: sequential sampling of seed bank
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
